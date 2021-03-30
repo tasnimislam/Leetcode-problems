952e689dabad5bb8004f767e73ff373add4e2fbe
Tasnim Nishat Islam <46325413+tasnimislam@users.noreply.github.com>
2021-03-31 03:31:04 +0600

    Update check_word_list.py

--- check_word_list.py
+++ check_word_list.py
@@ -8,18 +8,17 @@ class Solution(object):
         :rtype: List[str]
         """
         vowel = ['a', 'e', 'i', 'o', 'u']
-        quer_list_ind = 0
-        new_word_list = []
-        while(quer_list_ind<len(queries)):
-            word_list_ind = 0
-            new_word = ""
-            while(word_list_ind<len(words)):
-                q_ind = 0
-                w_ind = 0
-                while(q_ind<len(queries[quer_list_ind]) and w_ind<len(word[word_list_ind])):
-                     if (queries[quer_list_ind][q_ind].lower() not in vowel) and queries[quer_list_ind][q_ind].lower()!= wordlist[word_list_ind][w_ind].lower():
-                            word_list_ind +=1
-                            break
-            new_word_list.append(new_word)
+        new_quer_list = []
+        for quer in queries:
+            for word in wordlist:
+                if quer==word:
+                    new_quer_list.append(quer)
+                else:
+                    continue
+                    
+        return new_quer_list
+                    
+                        
+                            
                         
                             
