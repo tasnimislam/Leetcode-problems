8422f61c1561bd6537cba96412878b1ca054fcc4
Tasnim Nishat Islam <46325413+tasnimislam@users.noreply.github.com>
2021-04-03 20:38:38 +0600

    Update binary_tree_level.py

--- binary_tree_level.py
+++ binary_tree_level.py
@@ -6,13 +6,43 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
     def levelOrder(self, root):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        if root==None:
+            return []
+        
+        que = []
+        que.append(root)
+        que.append(2000)
         r_list = []
-        r_list.append([root.val])
-        r_list.append([root.left.val, root.right.val])
-        return levelOrder(root.left)
-        return levelOrder(root.right)
+        temp = []
+        while(len(que)!=0):
+            value = que[0]
+            que.pop(0)
+            if value==2000:
+                if len(que)==0:
+                    r_list.append(temp)
+                    break
+                r_list.append(temp)
+                temp = []
+                que.append(2000)
+                
+            else:
+                temp.append(value.val)
+                if value.left!=None:
+                    que.append(value.left)
+                if value.right!=None:
+                    que.append(value.right)
+        return r_list
+            
+        
