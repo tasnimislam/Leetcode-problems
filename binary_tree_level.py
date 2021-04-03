080c38213ea759d8c34c4fa844091617dc25fa98
Tasnim Nishat Islam <46325413+tasnimislam@users.noreply.github.com>
2021-04-03 20:39:06 +0600

    Update binary_tree_level.py

--- binary_tree_level.py
+++ binary_tree_level.py
@@ -5,7 +5,6 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
     # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
